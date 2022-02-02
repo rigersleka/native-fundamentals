31b844972a32438bebf23b575733778b5bfc5dd1
Rigers Leka <rigersleka2@gmail.com>
2022-02-02 12:23:45 +0100

    importance of using lifecycle gesture  system

--- App.js
+++ App.js
@@ -12,48 +12,78 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName='App_to_Home'
-          screenOptions={{
-            headerTintColor: 'white',
-            headerStyle: { backgroundColor: 'orange' },
-          }}
-        >
-          <Stack.Screen
-            name='App_to_Home'
-            component={HomeScreen}
-            options={{
-              title: 'Movie Time',
-              headerTitleStyle: {
-                fontWeight: 'bold',
-                fontSize: 30,
-              },
-            }}
-          />
-          <Stack.Screen
-            name='Home_to_Details'
-            component={DetailsScreen}
-            options={({ route }) => ({ title: route.params.movie.title })}
-          />
-          <Stack.Screen
-            name='Details_to_Details'
-            component={DetailsScreen}
-            options={({ route }) => ({ title: route.params.movie.title })}
-          />
-          <Stack.Screen name='BigImageView' component={ImageScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
+    <View
+      style={sty.Root_View}
+      onStartShouldSetResponder={() => {
+        console.log('1st view should set');
+        return false;
+      }}
+    >
+      <Text style={sty.rt}>1st (Root) View</Text>
+      <View
+        style={sty.Second_View}
+        onStartShouldSetResponder={() => {
+          console.log('2nd view should set?');
+          return true;
+        }}
+        onResponderGrant={() => {
+          console.log('2nd view granted response');
+        }}
+        onResponderRelease={() => {
+          console.log('2nd view released');
+        }}
+        onMoveShouldSetResponder={() => {
+          console.log('2nd view should set move');
+        }}
+        onResponderMove={() => {
+          console.log('2nd view responder move');
+        }}
+      >
+        <Text style={sty.st}>2nd View</Text>
+      </View>
+      <View
+        style={sty.Third_View}
+        onStartShouldSetResponder={() => {
+          console.log('3rd view should set?');
+          return false;
+        }}
+      >
+        <Text style={sty.tt}>3rd View</Text>
+      </View>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const sty = StyleSheet.create({
+  Root_View: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'aliceblue',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 5,
   },
+  rt: { fontSize: 20, fontWeight: 'bold' },
+  Second_View: {
+    flex: 0.7,
+    marginTop: 50,
+    marginLeft: 70,
+    backgroundColor: 'burlywood',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 5,
+    borderColor: 'white',
+  },
+  st: { fontSize: 20, color: 'white', fontWeight: 'bold' },
+  Third_View: {
+    marginTop: 50,
+    marginLeft: 70,
+    width: 300,
+    height: 300,
+    backgroundColor: 'chocolate',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 5,
+    borderColor: 'white',
+  },
+  tt: { fontSize: 20, color: 'white', fontWeight: 'bold' },
 });
